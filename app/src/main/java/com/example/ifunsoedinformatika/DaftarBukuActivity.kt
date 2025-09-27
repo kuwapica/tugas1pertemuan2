769d1daad3bd559c5c6f44038f4bd119d414bdc1
kuwapica <annidaaiska@gmail.com>
2025-09-28 02:34:16 +0700

    Tugas 3 Pertemuan 4

---
+++ app/src/main/java/com/example/ifunsoedinformatika/DaftarBukuActivity.kt
@@ -0,0 +1,30 @@
+package com.example.ifunsoedinformatika
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ifunsoedinformatika.databinding.ActivityDaftarBukuBinding
+import com.example.ifunsoedinformatika.ui.adapter.BookAdapter
+import com.example.ifunsoedinformatika.viewmodel.MainViewModel
+
+class DaftarBukuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDaftarBukuBinding
+    private val viewModel: MainViewModel by viewModels()
+    private val adapter = BookAdapter(emptyList())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDaftarBukuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+        viewModel.books.observe(this) {
+            adapter.setData(it)
+        }
+
+        viewModel.fetchBooks("kotlin programming")
+    }
+}
---
+++ app/src/main/java/com/example/ifunsoedinformatika/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.ifunsoedinformatika.utils
+
+object Constants {
+    const val BASE_URL = "https://openlibrary.org"
+}
